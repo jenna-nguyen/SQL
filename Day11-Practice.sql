73a39f31397190d787c4c8bf1c3ef9f96ab917dd
jenna-nguyen <161226789+jenna-nguyen@users.noreply.github.com>
2024-03-07 21:32:44 +0700

    Update Day11-Practice.sql

--- Day11-Practice.sql
+++ Day11-Practice.sql
@@ -89,5 +89,65 @@ join country on CITY.CountryCode=COUNTRY.Code
 group by COUNTRY.Continent;
 
 --EX2:
+SELECT
+round(count(texts.email_id)::decimal/count(distinct emails.email_id)::decimal,2) as activation_rate
+FROM emails
+LEFT JOIN texts on emails.email_id=texts.email_id and texts.signup_action='Confirmed';
 
+--EX3:
+SELECT
+age_breakdown.age_bucket,
+round(sum(case when activities.activity_type='send' then activities.time_spent else 0 END)/
+(sum(case when activities.activity_type='open' then activities.time_spent ELSE 0 END)+
+sum(case when activities.activity_type='send' then activities.time_spent else 0 END))*100,2) 
+as send_perc,
+round(sum(case when activities.activity_type='open' then activities.time_spent ELSE 0 END)/
+(sum(case when activities.activity_type='open' then activities.time_spent ELSE 0 END)+
+sum(case when activities.activity_type='send' then activities.time_spent else 0 END))*100,2) 
+as open_perc
+FROM activities
+join age_breakdown on age_breakdown.user_id=activities.user_id
+group by age_breakdown.age_bucket;
+
+--EX4:
+with Supercloud_Customer as (SELECT
+customer_contracts.customer_id as customer_id,
+count(distinct products.product_category) as count_category
+FROM customer_contracts
+join products on customer_contracts.product_id=products.product_id
+group by customer_contracts.customer_id)
+
+select 
+customer_id
+from Supercloud_Customer
+WHERE count_category=(select count(distinct product_category) from products);
+
+--EX5:
+select 
+emp1.employee_id,
+emp1.name,
+count(emp2.reports_to) as reports_count,
+round(avg(emp2.age),0) as average_age
+from Employees as emp1
+left join Employees as emp2 on emp1.employee_id=emp2.reports_to
+group by emp1.employee_id,emp1.name
+having count(emp2.reports_to)<>0;
+
+--EX6:
+select 
+Products.product_name,
+sum(orders.unit) as unit
+from Products
+join orders on products.product_id=orders.product_id
+where extract(year from orders.order_date)=2020 and extract(month from orders.order_date)=2
+group by products.product_name
+having sum(orders.unit)>=100;
+
+--EX7:
+SELECT pages.page_id
+FROM pages
+left join page_likes on pages.page_id=page_likes.page_id
+group by pages.page_id
+having count(page_likes.user_id)=0
+order by pages.page_id;
 
