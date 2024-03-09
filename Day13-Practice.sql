39c8db0da8fa57ab958f67da8205877c2465d33c
jenna-nguyen <161226789+jenna-nguyen@users.noreply.github.com>
2024-03-10 01:05:10 +0700

    Update Day13-Practice.sql

--- Day13-Practice.sql
+++ Day13-Practice.sql
@@ -36,4 +36,94 @@ SELECT
 total.category,total.product,total.total_spend
 from total as tbl1
 join total as tbl2 
-on tbl1.category=tbl2.category
+on tbl1.category=tbl2.category;
+
+--EX3:
+with call_record AS
+(SELECT policy_holder_id,
+count(case_id) as count_call
+FROM callers
+group by policy_holder_id)
+
+select count(policy_holder_id)
+from call_record
+where count_call>=3;
+
+
+--EX4:
+SELECT pages.page_id
+FROM pages
+left join page_likes on pages.page_id=page_likes.page_id
+group by pages.page_id
+having count(page_likes.user_id)=0
+order by pages.page_id;
+
+--EX5:
+with tbl1 AS
+(select user_id,
+extract(month from event_date) as month,
+count(event_id)
+from user_actions
+where extract(year from event_date)=2022 
+and extract(month from event_date)=6
+group by user_id, extract(month from event_date)
+having count(event_id)>0),
+
+tbl2 AS
+(select user_id,
+extract(month from event_date) as month,
+count(event_id)
+from user_actions
+where extract(year from event_date)=2022 
+and extract(month from event_date)=7
+group by user_id, extract(month from event_date)
+having count(event_id)>0)
+
+SELECT
+tbl2.month,
+count(DISTINCT tbl2.user_id)
+from tbl2
+join tbl1 on tbl1.user_id=tbl2.user_id
+group by tbl2.month;
+
+--EX6:
+with tbl1 as 
+(select
+extract(year from trans_date)||'-'||extract(month from trans_date) as month,
+COUNTRY,
+count(state) as approved_count,
+sum(amount) as approved_total_amount
+from transactions
+where state='approved'
+group by country, month),
+
+tbl2 as
+(select *,
+extract(year from Transactions.trans_date)||'-'||extract(month from Transactions.trans_date) as month
+from Transactions)
+
+select 
+tbl2.month,
+tbl2.country,
+count(tbl2.id) as trans_count,
+tbl1.approved_count,
+sum(tbl2.amount) as trans_total_amount,
+tbl1.approved_total_amount
+from Tbl2
+join tbl1 on tbl1.month=tbl2.month and tbl2.country=tbl1.country
+group by tbl2.month,tbl2.country,tbl1.approved_count,tbl1.approved_total_amount;
+
+--EX7:
+with tbl1 as
+(select product_id, min(year) as min_year from sales group by product_id)
+
+select distinct sales.product_id,
+sales.year as first_year,
+sales.quantity,
+sales.price
+from sales
+join tbl1 on tbl1.product_id=sales.product_id
+where sales.year in (select min_year from tbl1);
+
+--EX8:
+
