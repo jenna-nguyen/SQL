c809308eeb0fd6a0c57c0509824f1ef583d34b71
jenna-nguyen <161226789+jenna-nguyen@users.noreply.github.com>
2024-03-13 00:35:47 +0700

    Update Day15-Practice.sql

--- Day15-Practice.sql
+++ Day15-Practice.sql
@@ -1,1 +1,14 @@
+--EX1:
+with tbl1 AS
+(SELECT EXTRACT(year from transaction_date) as year,
+product_id,
+spend as curr_year_spend,
+lag(spend) over(PARTITION BY product_id order by product_id) 
+as prev_year_spend
+FROM user_transactions)
+select year,product_id,curr_year_spend,prev_year_spend,
+round((curr_year_spend-prev_year_spend)/prev_year_spend*100,2) as yoy_rate
+from tbl1;
+
+--EX2:
 
